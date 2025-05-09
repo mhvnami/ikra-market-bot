9d53cc613bcf0b628e3121c9a799dcba4df65270
mhvnami <mikhailovanata95@gmail.com>
2025-05-09 16:23:55 +0400

    код

--- bot.py
+++ bot.py
@@ -360,7 +360,12 @@ async def main():
     return app
 
 if __name__ == "__main__":
-    web.run_app(main(), host="0.0.0.0", port=int(os.getenv("PORT", 8080)))
+    import asyncio
+
+    app = asyncio.run(main())  # создаём приложение
+    web.run_app(app, host="0.0.0.0", port=int(os.getenv("PORT", 8080)))  # запускаем сервер
+
+
 
 
 
